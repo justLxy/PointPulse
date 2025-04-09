897396a68b0aef45c3450d939390f3184b493750
Xuanyi Lyu <xuanyi.lyu@mail.utoronto.ca>
2025-04-09 12:20:39 -0400

    Deployment

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-const port = (() => {
-    const args = process.argv;
-
-    if (args.length !== 3) {
-        console.error("usage: node index.js port");
-        process.exit(1);
-    }
-
-    const num = parseInt(args[2], 10);
-    if (isNaN(num)) {
-        console.error("error: argument must be an integer.");
-        process.exit(1);
-    }
-
-    return num;
-})();
+const port = process.env.PORT || 8000; // Use PORT env var or default to 8000
 
 // Configuration
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000"; // This can be changed when deployed
