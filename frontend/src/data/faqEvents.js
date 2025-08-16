5fc63fef701004227ba308b8d2b06ee44a1e131a
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2025-08-16 19:44:53 -0400

    add faq for events

---
+++ frontend/src/data/faqEvents.js
@@ -0,0 +1,36 @@
+// Frequently Asked Questions Data - Events
+// Last updated: 2025-08-16
+// -----------------------------------------
+
+const faqEvents = [
+    {
+        question: "How do I sign up for an event?",
+        answer: "Visit the 'Events' page to browse all published events. You can view details and RSVP for any event that still has available capacity."
+    },
+    { 
+        question: "Why do some events have limited capacity?",
+        answer: "Some events have limited capacity to ensure a more intimate and engaging experience for attendees. This allows for better interaction and participation. Other events may have an unlimited capacity as they are held in larger venues or are designed to accommodate more participants."
+    },
+    {
+        question: "How can I check-in during an event?",
+        answer: "Depending on how the event is organized, you can check-in by showing your QR code at the entrance, and have it scanned by an event organizer. You may also be able to check-in by using your device (logged into your PointPulse account) to scan a QR code that is accessible to all attendees."
+    },
+    {
+        question: "What happens if an event is canceled?",
+        answer: "If an event is canceled, all registered attendees will be notified via email."
+    },
+    {
+        question: "How are points awarded at events?",
+        answer: "Event organizers can award points to attendees based on their participation and engagement during the event. Points can be earned by simply checking-in on the day of, or by participating in activities and discussions."
+    },
+    {
+        question: "Why is it that I didn't receive points for an event?",
+        answer: "Points may not be awarded for various reasons, such as not checking in during the event, or not participating in the required activities. If you believe you should have received points, please contact support."
+    },
+    {
+        question: "What should I do if I can't attend an event?",
+        answer: "If you can't attend an event you've signed up for, please cancel your RSVP so that someone else may take your spot."
+    }
+]
+
+export default faqEvents;
