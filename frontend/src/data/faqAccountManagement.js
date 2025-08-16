4ae5bca45c3a02e4faea3d8d448e343001e231e3
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2025-08-16 19:07:06 -0400

    fork account management into own file and modify

---
+++ frontend/src/data/faqAccountManagement.js
@@ -0,0 +1,32 @@
+// Frequently Asked Questions Data - Account Management
+// Last updated: 2025-08-16
+// -----------------------------------------
+
+const faqAccountManagement = [
+    {
+        question: "I forgot my password. What should I do?",
+        answer: "On the login page, click 'Forgot Password' and enter your UTORid. A password reset token will be generated for you, which you can use on the reset page to set a new password."
+    },
+    {
+        question: "How can I update my personal information?",
+        answer: "Once logged in, navigate to your profile page. From there, you can update your name, display email, birthday, and profile avatar."
+    },
+    {
+        question: "Why is my account not verified?",
+        answer: "Account verification is performed by a Manager to confirm your status as a current member of the University of Toronto community. Verification is required to access certain features like point transfers."
+    },
+    {
+        question: "How do I verify my account?",
+        answer: "To verify your account, please visit the CSSU lounge (located in BA 2250) with your UTORid and a valid form of identification. A Cashier will assist you with the verification process."
+    },
+    {
+        question: "Why can't I delete my own account?",
+        answer: "To ensure transaction integrity and maintain a clear audit trail, users cannot delete accounts on their own. Please email the support team (cssu@cdf.toronto.edu) for assistance. "
+    },
+    {
+        question: "Does the CSSU delete unverified or unused accounts?",
+        answer: "We reserve the right to delete accounts that are unverified, and accounts that are unused for more than 12 months. We also reserve the right to delete accounts that violate our terms of service."
+    }
+]
+
+export default faqAccountManagement;
